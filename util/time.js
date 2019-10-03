29ce32a701f243a03aad5acdadbc4002da22bfc8
RiceGnat <ricegnat@gmail.com>
2019-10-03 11:40:25 -0500

    plural/singular for time units

--- util/time.js
+++ util/time.js
@@ -1,22 +1,28 @@
+// jst offset
 const offset = 9;
 
+// get time in jst
 function now() {
     return shiftHours(Date.now(), offset);
+}
+
+function pluralize(value, unit) {
+    return `${value} ${unit}${value > 1 ? 's' : ''}`;
 }
 
 function duration(totalSeconds) {
     let remaining = totalSeconds;
     let units = [];
     const days = Math.floor(remaining / 86400);
-    if (days > 0) units.push(`${days} days`);
+    if (days > 0) units.push(pluralize(days, 'day'));
     remaining %= 86400;
     const hours = Math.floor(remaining / 3600);
-    if (hours > 0) units.push(`${hours} hours`);
+    if (hours > 0) units.push(pluralize(hours, 'hour'));
     remaining %= 3600;
     const minutes = Math.floor(remaining / 60);
-    if (minutes > 0) units.push(`${minutes} minutes`);
+    if (minutes > 0) units.push(pluralize(minutes, 'minute'));
     const seconds = Math.floor(remaining % 60);
-    if (seconds > 0) units.push(`${seconds} seconds`);
+    if (seconds > 0) units.push(pluralize(seconds, 'second'));
     return units.slice(0, 3).join(", ");
 }
 
